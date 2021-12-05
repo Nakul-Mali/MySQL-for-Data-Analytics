24712e3a1345cc9d1fc4a27561a7108827ec487c
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-12-05 23:40:18 +0530

    Add files via upload

---
+++ 1. Basic Quries.sql
@@ -0,0 +1,29 @@
+select (10+20) as addition;
+
+select (10-20) as Substraction;
+
+select (10*20) as Multipication;
+
+select (10/20) as Division;
+
+select length('India is my country') as Total_length; 
+
+select repeat('Nakul ',5);
+
+select upper('India') as Upper_function;
+
+select lower('InDia') as lower_function;
+
+select current_date();
+
+select curdate();
+
+select day(current_date());
+
+select current_time();
+
+select current_timestamp();
+
+select now();
+
+
