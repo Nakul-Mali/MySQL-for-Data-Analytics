79c6de40572d5451cf285a661dba50d2e54cc691
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-12-15 23:27:40 +0530

    Add files via upload

---
+++ 10. Comment in SQL.sql
@@ -0,0 +1,13 @@
+
+/* Multi line
+Comment in SQL */
+
+/* Customer table contain all the details of the customer such as the Customer_ID,Customer_Name,
+Segment, Age, Country, City, State, Postal_Code and Region */
+select * from customer;
+
+-- Single line Comment
+
+-- to see Cusomers table Query write as
+select * from customer;
+
