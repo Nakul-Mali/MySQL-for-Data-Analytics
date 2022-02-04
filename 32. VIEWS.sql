b05db3507eb3b6c83bc91387ac4234b908befc0c
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2022-02-04 23:38:11 +0530

    Add files via upload

---
+++ 32. VIEWS.sql
@@ -0,0 +1,15 @@
+
+/* VIEW is not a physical table, it is a virtual table created by a query joining one or more tables*/
+
+-- syntax
+/*
+CREATE [OR REPLACE] VIEW view_name AS
+SELECT columns
+FROM tables
+[WHERE conditions];
+*/
+
+-- view is a query it is not a table but it behave like a table
+-- Whenever you look at a view it will contain containts as a table but it'll actully a query
+
+
